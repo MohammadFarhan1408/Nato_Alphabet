a42e9798d6a2ee472cf69ccfc02b5760ab93e44f
Mohammad Farhan <122987956+MohammadFarhan1408@users.noreply.github.com>
2024-06-24 09:52:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 
 alphabetic_data = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter the Name: ").upper()
-final_letter = [alphabetic_data[letter] for letter in word]
-print(final_letter)
+
+def generate_word():
+    word = input("Enter the Name: ").upper()
+    try:
+        final_letter = [alphabetic_data[letter] for letter in word]
+    except KeyError:
+        print("Sorry, Not a valid alphabet")
+        generate_word()
+    else:
+        print(final_letter)
+
+
+generate_word()
